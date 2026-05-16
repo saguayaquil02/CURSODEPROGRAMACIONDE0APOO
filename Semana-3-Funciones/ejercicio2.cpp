b1c38d56d39632ca6c43a1b83b30163556546d22
saguayaquil02 <saguayaquil02@gmail.com>
2026-05-15 22:34:37 -0500

    Clase 8 - Corregir los errores y ejecutar el programa - ej2

--- Semana-3-Funciones/ejercicio2.cpp
+++ Semana-3-Funciones/ejercicio2.cpp
@@ -2,7 +2,7 @@
 Tarea- Clase 8 - Ejercicio 2: Número mayor entre dos valores
 
 Autor: Sebastian Guayaquil
-Fecha: 11 de mayo del 2026
+Fecha: 15 de mayo del 2026
 
 Enunciado: Desarrolla un programa que permita ingresar dos números enteros y determine cuál
 de los dos es mayor.
@@ -57,40 +57,48 @@ using namespace std;
 
 // Prototipos de funciones
 // Colocamos solo nos nombres y parametros de las funciones que vamos a usar
-void leerNumeros(int num1, int num2);
-int compararValores(int num1,int num2);
-void mostrarResultados();
+void leerNumeros(int &num1, int &num2); // usamos paso por referencia
+int compararValores(int num1, int num2);
+void mostrarResultados(int num1, int num2, int resultado);
 
 int main(){
 
     // Declaramos variables para resultados
-    int num1, num2;
+    int num1, num2, resultado;
 
+    // Llamamos a la funcion para leer los numeros ingresados por el usuario
+    leerNumeros(num1, num2);
 
+    // Llamamos a la funcion compararValores y el retorno lo guardamos en una variable
+    resultado = compararValores(num1, num2);
+
+    mostrarResultados(num1, num2, resultado);
+
+    return 0;
+}
 
 
 
 // Esta funcion lee 2 numeros ingresados por el usuario
-void leerNumeros(int &num1, int &num2);{
+void leerNumeros(int &num1, int &num2){
 
-    cout << "Ingrese el primer numero: " << endl;
+    cout << "Ingrese el primer numero: ";
     cin >> num1;
-    cout << "Ingrese el segundo numero: " << endl;
+    cout << "Ingrese el segundo numero: ";
     cin >> num2;
 }
 
 // esta funcion calula la suma de los numeros ingresados en el arreglo 
+
 int compararValores(int num1, int num2){
 
-    // Condicion para comparar los numeros
-    if (num1 > num2)
-    {
-        int n1 = num1;
-    }else if (num1 == num2){
-        int igual= num2;
-    }else{
-        
-    }
+    if(num1 > num2)
+        return 1;
+    else if(num2 > num1)
+        return -1;
+    else
+        return 0;
+}
 
 // esta funcion muestra los resultados obtenidos
 void mostrarResultados(int num1, int num2, int resultado){
