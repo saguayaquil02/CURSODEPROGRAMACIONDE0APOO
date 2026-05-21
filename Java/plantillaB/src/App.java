03d5e5e1735d41de3884702aeb413dea4f651a90
saguayaquil02 <saguayaquil02@gmail.com>
2026-05-20 21:22:36 -0500

    cLASE 8- crear plantilla B Java

--- Java/plantillaB/src/App.java
+++ Java/plantillaB/src/App.java
@@ -1,7 +1,7 @@
 /*
 Título plantilla B - creacion de objetos 
 Autor: Sebastian Guayaquil
-Enunciado: Crear un objeto de tipo Persona, asignar valores a sus y mostrar su informacion a clase
+Enunciado: Crear un objeto de tipo Persona, asignar valores a sus atributos y mostrar su informacion a clase
 */
 public class App {
     public static void main(String[] args) throws Exception { // Método principal
@@ -10,7 +10,7 @@ public class App {
         persona1.nombre = "Carlos"; // Asignamos un valor al atributo nombre del objeto
         persona1.edad = 21; // Asignamos un vlaor al atributo edad del objeto
 
-        System.out.println("Nombre: " + persona1.nombre);
-        System.out.println("Edad: " + persona1.edad);    
+        System.out.println("Nombre: " + persona1.nombre); // Mostramos el nombre en consola
+        System.out.println("Edad: " + persona1.edad); // Mostramos la edad en consola   
     }
 }
--- Java/plantillaB/src/Persona.java
+++ Java/plantillaB/src/Persona.java
@@ -1,7 +1,7 @@
 /*
 Título: Plantilla B - Clase simple en Java
 Autor: Sebastian Guayaquil
-Enunciado: Crear un objeto de tipo Persona, asignar valores a sus y mostrar su informacion a clase
+Enunciado: Crear un objeto de tipo Persona, asignar valores a sus atributos y mostrar su informacion a clase
 */
 public class Persona { // Aqui declaramos una calse llamada Persona
     String nombre; // Atributo que guarda el nombre de la persona
