0bb2c8c2293ab9918d2bf4ee7439045b74d95a18
saguayaquil02 <saguayaquil02@gmail.com>
2026-05-20 20:51:31 -0500

    Clase 13 - crear archivo plantilla A

---
+++ Clase12/correccion_Examen.cpp
@@ -0,0 +1,13 @@
+/*
+CORRECCION DEL EXAMEN 1
+Autor:Sebastian Guayaquil
+Fecha: 18 de mayo del 2026
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// PROTOTIPO DE FUNCIONES
+int mostrarMenu();
+void mostrarListado(string nombres[], float notas[], int &cantidad, bool &hayDatos);
---
+++ Java/plantilla/src/App.java
@@ -0,0 +1,14 @@
+/*
+Titulo: Plantilla A - Estrcutura basica de java
+Autor: Sebastian Guayaquil
+Enunciado: Vamos a msotrar la estrcutua minima de un programa en java, para enternder su sintaxis principal antes de trabajar con clases y objetos.
+
+*/
+
+public class App { // Aqui declaramos una clase publica llamada App
+    public static void main(String[] args){ // Este es el metodo principal: el programa inicia
+        System.out.println("Hello, World!"); // Esta línea imprime un mensaje en consola
+    } // Aqui termina el metodo main
+} // Aqui termina la clase App
+
+
