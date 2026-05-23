d3f92ebd7d0d25c0518a133bb6f52e56a0b48313
saguayaquil02 <saguayaquil02@gmail.com>
2026-05-22 19:10:58 -0500

    Clase 14 - crear y completar archivo Gato.java

--- Java/ejercicio4/src/Gato.java
+++ Java/ejercicio4/src/Gato.java
@@ -1,13 +1,20 @@
 /*
-Archivo Gato
+Archivo: Gato.java
+Descripcion:
+Clase hija que hereda de Animal.
+Representa a un gato dentro del refugio
 */
 
-public class Gato {
+public class Gato extends Animal {
+    // Constructor de Gato
     public Gato(String nombre, int edad){
+        // Llamamos al constructor de la clase padre
         super(nombre, edad);
     }
     
     // Sobreescribimos el metodo hacerSonido()
     @Override
-    public void 
+    public void hacerSonido(){
+        System.out.println("Miau Miau");
+    }
 }
