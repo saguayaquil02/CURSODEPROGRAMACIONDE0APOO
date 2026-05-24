7379e8d6cf9cf2b117f977c2de021462b39878b7
saguayaquil02 <saguayaquil02@gmail.com>
2026-05-23 19:20:00 -0500

    Tarea Clase 13 - Ej01 crear clase Mascota, atributos y metodos

---
+++ Java/Ej01/src/Mascota.java
@@ -0,0 +1,27 @@
+/*
+Archivo: Mascota.java
+Descricion:
+Clase padre que presenta una mascota en general
+Aqui colocamos lo que todos los animales tienen en comun
+*/
+public class Mascota {
+    // Atributos privados
+    protected String nombre;
+    protected String especie;
+    protected int edad;
+
+    // Constructor
+    public Mascota(String nombre, String especie, int edad){
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+    }
+
+    // Metodos
+    // Mostrar la informacion de la mascota
+    public void mostrarInfo(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad);
+    }
+}
